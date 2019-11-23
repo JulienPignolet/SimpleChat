bd0a7514d27fe06d130d65c250647686b7a8787c
yoanf <yoanfath@laposte.net>
2019-11-23 10:30:47 +0100

    Ajout données au lancement de l'application

--- src/main/java/univ/lorraine/simpleChat/SimpleChat/repository/RoleRepository.java
+++ src/main/java/univ/lorraine/simpleChat/SimpleChat/repository/RoleRepository.java
@@ -1,11 +1,13 @@
 package univ.lorraine.simpleChat.SimpleChat.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import univ.lorraine.simpleChat.SimpleChat.model.Role;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
 	Optional<Role> findById(Long id);
-	Role findByName(String name); 
+	Role findByName(String name);
+	List<Role> findAll();
 }
