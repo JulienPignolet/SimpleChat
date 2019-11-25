8607d6cc35cf40db8be30de98ae459de6ca8d46b
h-wb <dev@wehbe.pro>
2019-11-26 00:35:28 +0100

    Update WebSecurityConfig.java

--- src/main/java/univ/lorraine/simpleChat/SimpleChat/security/WebSecurityConfig.java
+++ src/main/java/univ/lorraine/simpleChat/SimpleChat/security/WebSecurityConfig.java
@@ -83,8 +83,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
                 "/registration",
                 "/authentication/**",
                 "/h2-console/**").permitAll().
-                anyRequest().authenticated().and().headers().frameOptions().sameOrigin().and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                anyRequest().authenticated().and().headers().frameOptions().sameOrigin()
+                .and().cors()
+                .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
