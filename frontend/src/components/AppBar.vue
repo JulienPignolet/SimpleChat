8f971dbd9a6a5deaa9b70ecb77c7d42cd132a8f7
h-wb <dev@wehbe.pro>
2020-01-05 19:15:59 +0100

    :fire: Supression bouton déconnexion depuis la page de login et d'enregistrement

--- frontend/src/components/AppBar.vue
+++ frontend/src/components/AppBar.vue
@@ -3,10 +3,6 @@
     <img src="../../public/images/logo.svg" alt="logo simple chat" class="logo" height="40" />
     <v-spacer></v-spacer>
 
-    <v-btn v-if="!userIsConnected()" icon @click="$router.push('/login')">
-      <v-icon large color="white">mdi-login</v-icon>
-    </v-btn>
-
     <v-btn v-if="userIsConnected()" icon @click="deconnexion()">
       <v-icon large color="white">mdi-power</v-icon>
     </v-btn>
